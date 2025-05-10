930ce158271a92477c32e6a87f86928360e0e7a5
nikhilachalkar <142605075+nikhilachalkar@users.noreply.github.com>
2025-05-10 12:31:18 +0530

    LLM based Project

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use(cors({
 }));
 app.use(cookieParser());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
 
